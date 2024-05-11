aa01d91e8ad9a3b36cc27a140f294fe3317938fa
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-05-11 20:23:05 -0300

    initial commit

---
+++ src/main/java/br/com/postech/techchallengeorder/TechchallengeOrderApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallengeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallengeOrderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TechchallengeOrderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/postech/techchallengeorder/TechchallengeOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallengeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallengeOrderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
